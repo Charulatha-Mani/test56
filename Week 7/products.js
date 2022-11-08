0388b61d13b5a5dd2f31de9e816f5cd5b49b7d25
Charulatha Mani <charulathamani118@gmail.com>
2022-11-08 10:08:58 +0400

    new commit god

---
+++ Week 7/products.js
@@ -0,0 +1,38 @@
+let products = [
+    {
+        id: 1001,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistible</em>," + "organic goodness for your cat.",
+        price: 2000,
+        image: "images/catfood.jpg",
+        availableInventory: 10,
+        rating: 3,
+    },
+    {
+        id: 1002,
+        title: "Yarn",
+        description: "Yarn your cat can play with for a very <strong>long</strong> time",
+        price: 2.99,
+        image: "images/yarn.png",
+        availableInventory: 10,
+        rating: 2,
+    },
+    {
+        id: 1003,
+        title: "Ball",
+        description: "Bounce your cat with a ball",
+        price: 5.00,
+        image: "images/ball.jpg",
+        availableInventory: 10,
+        rating: 1,
+    },
+    {
+        id: 1004,
+        title: "Food bowl",
+        description: "Your cat deserves a bowl",
+        price: 10.99,
+        image: "images/bowl.jpg",
+        availableInventory: 10,
+        rating: 4,
+    }
+]
